2e4b19f3fc1fc9d9338647b0bf4d42db315de5c2
aishokoo <yadianayat@gmail.com>
2025-12-14 13:15:28 +0200

    Version 1.0

--- app/src/main/java/com/example/webapp/MainActivity.kt
+++ app/src/main/java/com/example/webapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.os.Build
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,18 @@ class MainActivity : AppCompatActivity() {
         val webView = findViewById<WebView>(R.id.webView)
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
+        webView.settings.setSupportZoom(true)
+        webView.settings.builtInZoomControls = true
+
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webView.settings.mixedContentMode = android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
+
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("file:///android_asset/web/index.html")
+
+        webView.loadUrl("http://192.168.0.107:5000/")
     }
 }
